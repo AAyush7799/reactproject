55c3d82bcb9106cc385612ec7a74d23bdbd166b5
Aayush Tripathi <tripathiaayush797@gmail.com>
2021-06-06 20:24:12 +0530

    structure changed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react'
+import configureStore from './store';
+import { router } from "./router";
+import "babel-polyfill";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+const { store, persistor } = configureStore();
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  	<Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {router}
+      </PersistGate>
+    </Provider>,
   document.getElementById('root')
 );
 
